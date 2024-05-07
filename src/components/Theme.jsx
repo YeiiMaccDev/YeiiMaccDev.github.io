24649e4db47f28a980bb3eb5afc352246891641d
YeiiMaccDev <yeiimaccdev@gmail.com>
2024-05-07 12:19:43 -0500

    UseThemeContext for Changes the theme of the page according to the system theme change or user change.

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -1,55 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Theme.css'
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useThemeContext } from '../context/ThemeProvider';
 
 export const Theme = () => {
-    const isSystemDarkTheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    const isDarkOrLight = (theme) => theme ? "dark" : "light";
+    const { systemDarkTheme, darkTheme, handleChangeTheme } = useThemeContext();
     console.log("Theme");
-
-    // Theme used in the system to set a light or dark favicon.
-    const [systemDarkTheme, setSystemDarkTheme] = useState(isSystemDarkTheme)
-
-    // selected theme for the page
-    const [darkTheme, setDarkTheme] = useState(() => {
-        const selectedTheme = localStorage.getItem("selectedTheme") || isDarkOrLight(systemDarkTheme);
-        return (selectedTheme === "dark");
-    });
-
-    // Update page theme
-    const setTheme = () => {
-        document.getElementById("body").setAttribute("data-theme", isDarkOrLight(darkTheme));
-        localStorage.setItem("selectedTheme", isDarkOrLight(darkTheme));
-    }
-
-    // Change the theme of the page according to the theme change of the system.
-    const handleChangeSystemTheme = () => {
-        const windowDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
-        setDarkTheme(windowDarkTheme);
-        setSystemDarkTheme(windowDarkTheme);
-    }
-
-    // Change the theme of the page according to the user
-    const handleChangeTheme = () => {
-        setDarkTheme(!darkTheme);
-    }
-
-    // Change the theme of the page according to the user
-    useEffect(() => {
-        setTheme();
-    }, [darkTheme])
-
-    // Change the theme of the page according to the theme change of the system.
-    useEffect(() => {
-        const darkModeListener = window.matchMedia('(prefers-color-scheme: dark)');
-        darkModeListener.addEventListener('change', handleChangeSystemTheme);
-
-        return () => {
-            darkModeListener.removeEventListener('change', handleChangeSystemTheme);
-        };
-    }, []);
-
 
     return (
         <div className="switcher-container">
