4f367d22a0fc3d53c572856b530ec3b84f777efc
YeiiMaccDev <yeiimaccdev@gmail.com>
2024-05-16 23:05:34 -0500

    Create object with projects, for dynamic rendering.

--- src/pages/sections/ProjectSection.jsx
+++ src/pages/sections/ProjectSection.jsx
@@ -2,11 +2,47 @@
 import './ProjectSection.css'
 
 import projectLT from '../../assets/img/proyects/LTC1.png'
-import projectSP1 from '../../assets/img/proyects/SP1.png'
 import projectMS from '../../assets/img/proyects/MS1.png'
+import projectSP1 from '../../assets/img/proyects/SP1.png'
 import projectECO from '../../assets/img/proyects/ECO1.png'
 
+const poyects = [
+    {
+        title: 'Lifetime Capital - Financial Group',
+        images: [projectLT],
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        tecnologies: ['HTML', 'CSS', 'React.js', 'Bootstrap'],
+        demo: 'https://lifetimecapital.com',
+        github: 'https://github.com/joswald123/Lifetimews'
+    },
+    {
+        title: 'Moda Sport',
+        images: [projectMS],
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        tecnologies: ['HTML', 'CSS', 'Bootstrap', 'React.js', 'Node.js',],
+        demo: 'yeiimaccdev.github.io/ModaSport/',
+        github: 'https://github.com/YeiiMaccDev/ModaSport'
+    },
+    {
+        title: 'ShoesPlay',
+        images: [projectSP1],
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        tecnologies: ['HTML', 'CSS', 'Material UI', 'React.js', 'Node.js',],
+        demo: 'yeiimaccdev.github.io/ModaSport/',
+        github: 'https://github.com/YeiiMaccDev/ShoesPlay'
+    },
+    {
+        title: 'Tecno',
+        images: [projectECO],
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        tecnologies: ['HTML', 'CSS', 'React.js', 'Node.js',],
+        demo: 'yeiimaccdev.github.io/E-commerce_Js/',
+        github: 'https://yeiimaccdev.github.io/E-commerce_Js/'
+    }
+]
+
 export const ProjectSection = () => {
+
     return (
         <section className="projects section-100vh" id="workHistory">
             <div className="projects__data container">
@@ -22,47 +58,22 @@ export const ProjectSection = () => {
                     </nav>
 
                     <div className="projects__list">
-                        <div className="projects__item">
-                            <div className="projects__item-img">
-                                <img src={projectLT} />
-                            </div>
-                            <hr />
-                            <div className="projects__item-description">
-                                <p><strong>Lifetime Capital - Financial Group</strong></p>
-                            </div>
-                        </div>
 
-                        <div className="projects__item">
-                            <div className="projects__item-img">
-                                <img src={projectMS} />
-                            </div>
-                            <hr />
-                            <div className="projects__item-description">
-                                <p><strong>Moda Sport</strong></p>
-                            </div>
-                        </div>
-                        <div className="projects__item">
-                            <div className="projects__item-img">
-                                <img src={projectSP1} />
-                            </div>
-                            <hr />
-                            <div className="projects__item-description">
-                                <p><strong>ShoesPlay</strong></p>
-                            </div>
-                        </div>
-                        <div className="projects__item">
-                            <div className="projects__item-img">
-                                <img src={projectECO} />
-                            </div>
-                            <hr />
-                            <div className="projects__item-description">
-                                <p><strong>Moda Sport</strong></p>
-                            </div>
-                        </div>
+                        {
+                            poyects.map(({ title, images }) => (
+                                <div className="projects__item" key={title}>
+                                    <div className="projects__item-img">
+                                        <img src={images[0]} />
+                                    </div>
+                                    <hr />
+                                    <div className="projects__item-description">
+                                        <p><strong>{title}</strong></p>
+                                    </div>
+                                </div>
+                            ))
+                        }
+
                     </div>
-
-
-
 
                 </div>
 
